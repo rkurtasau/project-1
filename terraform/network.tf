fae5ab8349723796e48c2c19471185dfdddc9405
rkurtasau <rkurtasov@gmail.com>
2025-08-06 18:13:21 +0000

    Subnets resources added

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,3 +5,19 @@ resource "google_compute_network" "vpc_network" {
     delete_default_routes_on_create = true
     description = "Main network for GCP resources"
 }
+
+resource "google_compute_subnetwork" "public_subnet" {
+    name = "public-subnet"
+    network = google_compute_network.vpc_network.id
+    ip_cidr_range = "10.0.1.0/26"
+    description = "A public subnet"
+    depends_on = [google_compute_network.vpc_network] 
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+    name = "private-subnet"
+    network = google_compute_network.vpc_network.id
+    ip_cidr_range = "10.0.3.0/26"
+    description = "A private subnet"
+    depends_on = [google_compute_network.vpc_network]
+}
