9dad5f0e255e0314f2dff47ef5181f1fcaba03c0
rkurtasau <rkurtasov@gmail.com>
2025-08-12 12:05:58 +0000

    network_ip resource attribute changed. nat_ip resource attribute changed. Firewall rule and external_ip resource attached to the app-instance. Metadata for ssh connection to the app-instance added. App-instance disk settings changed within disk resource deletion.

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -6,26 +6,28 @@ resource "google_compute_instance" "app_instance" {
   network_interface {
     network = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.subnet_1.id
-    network_ip = google_compute_address.internal_ip_app_instance.id
+    network_ip = google_compute_address.internal_ip_app_instance.address
     access_config {
-      nat_ip = google_compute_address.external_ip_app_instance.address
+      nat_ip = google_compute_address.external_ip_app_instance1.address
       network_tier = "STANDARD"
     }
   }
   boot_disk {
-    source = google_compute_disk.app_instance_disk.id 
+    initialize_params {
+      size = 30
+      type = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+    }
+    auto_delete = false
     }
   depends_on = [ 
     google_compute_subnetwork.subnet_1,
-    google_compute_disk.app_instance_disk ]
+    google_compute_address.external_ip_app_instance1,
+    google_compute_firewall.ssh_access ]
   allow_stopping_for_update = true
+  metadata = {
+    ssh-keys = "${var.user-ssh-app-instance}:${var.ssh-key-app-instance}"
+  }
   description = "Main instance to deploying the app"
-}
-
-resource "google_compute_disk" "app_instance_disk" {
-    name = "app-instance-disk"
-    type = "pd-standard"
-    image = "ubuntu-os-cloud/ubuntu-minimal-2404-lts-arm64"
-    size = 30
-    description = "The disk attached to the app_instance"
+  tags = [ "app-instance" ]
 }
