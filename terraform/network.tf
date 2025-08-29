35dac265cfec0f499a7738060c34dbfac4c7f6a2
rkurtasau <rkurtasov@gmail.com>
2025-08-29 08:42:07 +0000

    IP addresses resources added for app-docker-hosts 1-2, load-balancers 1-2 and lb L7

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,8 +16,8 @@ resource "google_compute_subnetwork" "private" {
 }
 
 
-resource "google_compute_address" "external_app1" {
-  name         = "external-app1"
+resource "google_compute_address" "external_lb_l7" {
+  name         = "external-lb-l7"
   address_type = "EXTERNAL"
   network_tier = "STANDARD"
   lifecycle {
@@ -27,11 +27,48 @@ resource "google_compute_address" "external_app1" {
 }
 
 
-resource "google_compute_address" "internal_app1" {
-  name         = "internal-app1"
+resource "google_compute_address" "internal_lb_l7" {
+  name         = "internal-lb-l7"
   address_type = "INTERNAL"
-  address      = "10.0.1.3"
+  address      = "10.0.1.10"
   subnetwork   = google_compute_subnetwork.private.id
+  description  = "L7 load balancer internal address"
+}
+
+
+resource "google_compute_address" "internal_lb1" {
+  name         = "internal-lb-1"
+  address_type = "INTERNAL"
+  address      = "10.0.1.11"
+  subnetwork   = google_compute_subnetwork.private.id
+  description  = "Load balancer 1(main) internal address"
+}
+
+
+resource "google_compute_address" "internal_lb2" {
+  name         = "internal-lb-2"
+  address_type = "INTERNAL"
+  address      = "10.0.1.12"
+  subnetwork   = google_compute_subnetwork.private.id
+  description  = "Load balancer 2(reserve) internal address"
+}
+
+
+resource "google_compute_address" "internal_adh1" {
+  name         = "internal-adh-1"
+  address_type = "INTERNAL"
+  address      = "10.0.1.21"
+  subnetwork   = google_compute_subnetwork.private.id
+  description  = "App docker-host-2 internal address"
+}
+
+
+resource "google_compute_address" "internal_adh2" {
+  name         = "internal-adh-2"
+  address_type = "INTERNAL"
+  address      = "10.0.1.22"
+  subnetwork   = google_compute_subnetwork.private.id
+  description  = "App docker-host-2 internal address"
 }
 
 
