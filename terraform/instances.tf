9cdd627c42d573f271cf1f6b215edb71dab84f5b
rkurtasau <rkurtasov@gmail.com>
2025-08-29 08:47:17 +0000

    app-instance changed to app-docker-host-1 instance and within settings applyed. App-docker-host-2 instance resource created

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,16 +1,12 @@
-resource "google_compute_instance" "app_instance" {
-  name         = "app-instance"
+resource "google_compute_instance" "app_docker_host_1" {
+  name         = "app-docker-host-1"
   machine_type = "e2-standard-2"
 
   network_interface {
     network    = google_compute_network.main.id
     subnetwork = google_compute_subnetwork.private.id
-    network_ip = google_compute_address.internal_app1.address
-    access_config {
-      nat_ip       = google_compute_address.external_app1.address
-      network_tier = "STANDARD"
+    network_ip = google_compute_address.internal_adh1.address
     }
-  }
 
   boot_disk {
     initialize_params {
@@ -19,41 +15,45 @@ resource "google_compute_instance" "app_instance" {
       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
     }
     auto_delete = false
-  }
-
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh"
-      host = google_compute_address.external_app1.address
-      user = var.user-ssh-app-instance
-      private_key = file("/home/asus/.ssh/id_ed25519")
     }
-    inline = [ 
-      "sudo apt update",
-      "sudo apt install -y python3",
-      "python3 --version"
-     ]
-  }
-
-  provisioner "remote-exec" {
-    connection {
-      type = "ssh"
-      host = google_compute_address.external_app1.address
-      user = var.user-ssh-app-instance
-      private_key = file("/home/asus/.ssh/id_ed25519")
-    }
-     script = "./docker-install.sh"
-  }
 
   depends_on = [
     google_compute_subnetwork.private,
-    google_compute_address.external_app1
+    google_compute_address.internal_adh1
     ]
-
   allow_stopping_for_update = true
   metadata = {
     ssh-keys = "${var.user-ssh-app-instance}:${var.ssh-key-app-instance}"
-  }
-  description = "Main instance to deploying the app"
-  tags        = ["app-instance"]
+    }
+  description = "Main docker-host application deploying"
+  tags        = ["app-docker-host-1"]
+}
+
+
+resource "google_compute_instance" "app_docker_host_2" {
+  name         = "app-docker-host-2"
+  machine_type = "e2-standard-2"
+  network_interface {
+    network    = google_compute_network.main.id
+    subnetwork = google_compute_subnetwork.private.id
+    network_ip = google_compute_address.internal_adh2.address
+    }
+  boot_disk {
+    initialize_params {
+      size  = 30
+      type  = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+      }
+    auto_delete = false
+    }
+  depends_on = [
+    google_compute_subnetwork.private,
+    google_compute_address.internal_adh2
+    ]
+  allow_stopping_for_update = true
+  metadata = {
+    ssh-keys = "${var.user-ssh-app-instance}:${var.ssh-key-app-instance}"
+    }
+  description = "Reserve docker-host application deploying"
+  tags        = ["app-docker-host-2"]
 }
