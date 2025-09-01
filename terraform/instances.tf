b41cad68097f7a93fd015269fc9214a0924f2d0d
rkurtasau <rkurtasov@gmail.com>
2025-09-01 11:29:50 +0000

    load-balancers resources created. Disk size for management host changed

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -6,7 +6,7 @@ resource "google_compute_instance" "app_docker_host_1" {
     network    = google_compute_network.main.id
     subnetwork = google_compute_subnetwork.private.id
     network_ip = google_compute_address.internal_adh1.address
-  }
+    }
 
   boot_disk {
     initialize_params {
@@ -15,16 +15,16 @@ resource "google_compute_instance" "app_docker_host_1" {
       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
     }
     auto_delete = false
-  }
+    }
 
   depends_on = [
     google_compute_subnetwork.private,
     google_compute_address.internal_adh1
-  ]
+    ]
   allow_stopping_for_update = true
   metadata = {
     ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
-  }
+    }
   description = "Main docker-host application deploying"
   tags        = ["app-docker-host-1"]
 }
@@ -37,23 +37,23 @@ resource "google_compute_instance" "app_docker_host_2" {
     network    = google_compute_network.main.id
     subnetwork = google_compute_subnetwork.private.id
     network_ip = google_compute_address.internal_adh2.address
-  }
+    }
   boot_disk {
     initialize_params {
       size  = 30
       type  = "pd-standard"
       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
-    }
+      }
     auto_delete = false
-  }
+    }
   depends_on = [
     google_compute_subnetwork.private,
     google_compute_address.internal_adh2
-  ]
+    ]
   allow_stopping_for_update = true
   metadata = {
     ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
-  }
+    }
   description = "Reserve docker-host application deploying"
   tags        = ["app-docker-host-2"]
 }
@@ -67,12 +67,12 @@ resource "google_compute_instance" "management_host" {
     subnetwork = google_compute_subnetwork.private.id
     network_ip = google_compute_address.internal_management.address
     access_config {
-      nat_ip = google_compute_address.external_management.address
+    nat_ip = google_compute_address.external_management.address
     }
   }
   boot_disk {
     initialize_params {
-      size  = 15
+      size  = 20
       type  = "pd-standard"
       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
     }
@@ -82,11 +82,102 @@ resource "google_compute_instance" "management_host" {
   metadata = {
     ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
   }
-  depends_on = [
+  depends_on = [ 
     google_compute_subnetwork.private,
     google_compute_address.internal_management,
     google_compute_address.external_management
   ]
   description = "Management host"
-  tags        = ["management-host"]
+  tags = [ "management-host" ]
+}
+
+
+resource "google_compute_instance" "lbL7" {
+  name         = "load-balancer-L7"
+  machine_type = "e2-standard-2"
+  network_interface {
+    network    = google_compute_network.main.id
+    subnetwork = google_compute_subnetwork.private.id
+    network_ip = google_compute_address.internal_lb_l7.address
+    access_config {
+    nat_ip = google_compute_address.external_lb_l7.address
+    }
+  }
+  boot_disk {
+    initialize_params {
+      size  = 10
+      type  = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+    }
+    auto_delete = false
+  }
+  allow_stopping_for_update = true
+  metadata = {
+    ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
+  }
+  depends_on = [ 
+    google_compute_subnetwork.private,
+    google_compute_address.internal_lb_l7,
+    google_compute_address.external_lb_l7
+  ]
+  description = "Load Balancer Layer 7 host"
+  tags = [  ]
+}
+
+
+resource "google_compute_instance" "lb1" {
+  name         = "load-balancer-1"
+  machine_type = "e2-standard-2"
+  network_interface {
+    network    = google_compute_network.main.id
+    subnetwork = google_compute_subnetwork.private.id
+    network_ip = google_compute_address.internal_lb1.address
+  }
+  boot_disk {
+    initialize_params {
+      size  = 10
+      type  = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+    }
+    auto_delete = false
+  }
+  allow_stopping_for_update = true
+  metadata = {
+    ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
+  }
+  depends_on = [ 
+    google_compute_subnetwork.private,
+    google_compute_address.internal_lb1
+  ]
+  description = "Load Balancer 1 host"
+  tags = [  ]
+}
+
+
+resource "google_compute_instance" "lb2" {
+  name         = "load-balancer-2"
+  machine_type = "e2-standard-2"
+  network_interface {
+    network    = google_compute_network.main.id
+    subnetwork = google_compute_subnetwork.private.id
+    network_ip = google_compute_address.internal_lb2.address
+  }
+  boot_disk {
+    initialize_params {
+      size  = 10
+      type  = "pd-standard"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+    }
+    auto_delete = false
+  }
+  allow_stopping_for_update = true
+  metadata = {
+    ssh-keys = "${var.user-ssh-adh}:${var.ssh-key-adh}"
+  }
+  depends_on = [ 
+    google_compute_subnetwork.private,
+    google_compute_address.internal_lb2
+  ]
+  description = "Load Balancer 2 host"
+  tags = [  ]
 }
