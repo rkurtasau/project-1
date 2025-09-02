27af9563dc40e3a7dae092cfd03ed6a5528aa56d
rkurtasau <rkurtasov@gmail.com>
2025-09-02 13:31:10 +0000

    adh2, lb2, lbl7 IP-address-resources deleted due to project-infrastructure changes

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,26 +16,6 @@ resource "google_compute_subnetwork" "private" {
 }
 
 
-resource "google_compute_address" "external_lb_l7" {
-  name         = "external-lb-l7"
-  address_type = "EXTERNAL"
-  network_tier = "STANDARD"
-  lifecycle {
-    create_before_destroy = true
-  }
-  depends_on = [google_compute_network.main]
-}
-
-
-resource "google_compute_address" "internal_lb_l7" {
-  name         = "internal-lb-l7"
-  address_type = "INTERNAL"
-  address      = "10.0.1.10"
-  subnetwork   = google_compute_subnetwork.private.id
-  description  = "L7 load balancer internal address"
-}
-
-
 resource "google_compute_address" "internal_lb1" {
   name         = "internal-lb-1"
   address_type = "INTERNAL"
@@ -45,28 +25,10 @@ resource "google_compute_address" "internal_lb1" {
 }
 
 
-resource "google_compute_address" "internal_lb2" {
-  name         = "internal-lb-2"
-  address_type = "INTERNAL"
-  address      = "10.0.1.12"
-  subnetwork   = google_compute_subnetwork.private.id
-  description  = "Load balancer 2(reserve) internal address"
-}
-
-
 resource "google_compute_address" "internal_adh1" {
   name         = "internal-adh-1"
   address_type = "INTERNAL"
   address      = "10.0.1.21"
-  subnetwork   = google_compute_subnetwork.private.id
-  description  = "App docker-host-2 internal address"
-}
-
-
-resource "google_compute_address" "internal_adh2" {
-  name         = "internal-adh-2"
-  address_type = "INTERNAL"
-  address      = "10.0.1.22"
   subnetwork   = google_compute_subnetwork.private.id
   description  = "App docker-host-2 internal address"
 }
